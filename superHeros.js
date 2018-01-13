5cd23b013965d6db3adac1eb4d774061b3e4cc5c
Sahana Balram <sahana2008balram@gmail.com>
2018-01-12 16:04:44 -0800

    Brands display

---
+++ superHeros.js
@@ -0,0 +1,17 @@
+var brands = ["Acer", "Apple", "Sony", "Samsung"];
+var heroes = [
+    "Black Panther",
+    "Cyborg",
+    "Black Canary",
+    "Donna Troy",
+    "Huntress",
+    "Blue Beetle",
+    "Captain Atom"
+];
+var booksOnMyShelf = ["Calculus Early Transcendentals", "Ravens", "The Self Illusion", "Harry Potter"];
+var thingsInFrontOfMe = ["Laptop", "Beanbag", "Cats", "Slippers"];
+var howIFeel = ["Sleep Deprived", "Wired on Coffee", "Excited"];
+
+for(var i = 0; i< brands.length; i++) {
+    console.log(brands[i]);
+}
