4b6ab06256fe386a84e66521ef8915e8fe59b679
Sahana Balram <sahana2008balram@gmail.com>
2018-01-12 16:03:25 -0800

    Solution

--- superHerosWithFunction.js
+++ superHerosWithFunction.js
@@ -1,5 +1,18 @@
 var brands = ["Acer", "Apple", "Sony", "Samsung"];
-      var heroes = ["Black Panther", "Cyborg", "Black Canary", "Donna Troy", "Huntress", "Blue Beetle", "Captain Atom"];
-      var booksOnMyShelf = ["Calculus Early Transcendentals", "Ravens", "The Self Illusion", "Harry Potter"];
-      var thingsInFrontOfMe = ["Laptop", "Beanbag", "Cats", "Slippers"];
-      var howIFeel = ["Sleep Deprived", "Wired on Coffee", "Excited"];
+var heroes = ["Black Panther", "Cyborg", "Black Canary", "Donna Troy", "Huntress", "Blue Beetle", "Captain Atom"];
+var booksOnMyShelf = ["Calculus Early Transcendentals", "Ravens", "The Self Illusion", "Harry Potter"];
+var thingsInFrontOfMe = ["Laptop", "Beanbag", "Cats", "Slippers"];
+var howIFeel = ["Sleep Deprived", "Wired on Coffee", "Excited"];
+
+function superHeros(arr) {
+    for(var i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+    console.log("----------------------------------------");
+}
+
+superHeros(brands);
+superHeros(heroes);
+superHeros(booksOnMyShelf);
+superHeros(thingsInFrontOfMe);
+superHeros(howIFeel);
